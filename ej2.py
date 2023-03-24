2b4679f9f3c29f96121b375133118d0d86d21be4
ferniprogrammer <fsanchor@myuax.com>
2023-03-24 11:35:24 +0100

    ej 1 y 2 terminados

---
+++ ej2.py
@@ -0,0 +1,18 @@
+class animal:
+    pass
+
+class vivíparo(animal):
+    pass
+
+class vertebrado(animal):
+    pass
+
+class mamífero(vertebrado):
+    pass
+
+class gato(mamífero, vivíparo):
+    pass
+
+    
+    
+
