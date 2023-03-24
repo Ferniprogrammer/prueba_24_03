28d23cbaa98a4fa6986c2c85f334bf2111d3065b
ferniprogrammer <fsanchor@myuax.com>
2023-03-24 11:35:35 +0100

    ej 1 y 2 terminados

---
+++ ej1.py
@@ -0,0 +1,9 @@
+class libro:
+    def libro(self):
+        self.nombre = str(input("Escriba el título del libro: "))
+        self.género = str(input("Escriba el género del libro: "))
+        self.long = str(input("Escriba el número de páginas: "))
+        self.editorial = str(input("Escriba la editorial donde fue publicado el libro: "))
+        self.autor =  str(input("escriba el nombre del autor: "))
+        self.fecha = str(input("escriba la fecha de publicación del libro: "))
+        
